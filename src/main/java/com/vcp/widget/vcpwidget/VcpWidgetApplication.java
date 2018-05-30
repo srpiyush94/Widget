fa669c31d5b98ed2eed379f242e018b60fac6360
Piyush <Piyush@DESKTOP-CN5CEA5>
2018-05-30 07:31:45 +0530

    Initial Commit

---
+++ src/main/java/com/vcp/widget/vcpwidget/VcpWidgetApplication.java
@@ -0,0 +1,12 @@
+package com.vcp.widget.vcpwidget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcpWidgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcpWidgetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vcp/widget/vcpwidget/VcpWidgetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vcp.widget.vcpwidget;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VcpWidgetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
